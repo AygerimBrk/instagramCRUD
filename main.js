4e5261a53e6ed0ba330bb592a74c68f7fff8f9b1
AygerimBrk <ajgerimsadirova907@gmail.com>
2023-02-28 11:46:51 +0600

    let input

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const API = "http://localhost:8000/Post";
+
+//? переменные для инпутов
+let profilePhoto = document.querySelector("#profile_photo_url");
+let nickName = document.querySelector("#nick_name");
+let region = document.querySelector("#region");
+let imageUrl = document.querySelector("#image_url");
+let countLike = document.querySelector("#likes");
+let comment = document.querySelector("#comments");
+let btnSend = document.querySelector(".btn");
+let postList = document.querySelector(".post_list");
